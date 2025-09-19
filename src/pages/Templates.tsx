4dabe17755a08afbca0b037283047240120b811d
SchweigyDev <Mschweighauser12@gmail.com>
2025-09-19 18:21:09 -0500

    fixed loading errors

--- src/pages/Templates.tsx
+++ src/pages/Templates.tsx
@@ -12,7 +12,7 @@ const Templates = () => {
             <h1 className="text-3xl font-bold mb-6">Templates</h1>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {templates.map((t, i) => (
-                    <UserCases key={i} {...t} />
+                    <UserCases title={""} image={""} key={i} {...t} />
                 ))}
             </div>
         </section>
